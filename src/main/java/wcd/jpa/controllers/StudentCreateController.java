1772fbadf3f5e08af3594ba8e0b7056439b776ad
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-02-01 14:15:52 +0700

    Send email

--- src/main/java/wcd/jpa/controllers/StudentCreateController.java
+++ src/main/java/wcd/jpa/controllers/StudentCreateController.java
@@ -13,11 +13,19 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import wcd.jpa.entities.Subjects;
 
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
+
 
 @WebServlet(value = "/create-student")
 public class StudentCreateController extends HttpServlet {
@@ -69,6 +77,42 @@ public class StudentCreateController extends HttpServlet {
 
             session.save(student);
             session.getTransaction().commit();
+
+            // Send email to student
+            String senderEmail = "sonnmth2205010@fpt.edu.vn";
+            String passwordEmail = "tpaw uayu arfn gecz";
+
+            // Config connect
+            Properties prop = new Properties();
+            prop.put("mail.smtp.auth","true");
+            prop.put("mail.smtp.ssl.protocols","TLSv1.2");
+            prop.put("mail.smtp.ssl.trust","smtp.gmail.com");
+            prop.put("mail.smtp.starttls.enable","true");
+            prop.put("mail.smtp.host","smtp.gmail.com");
+            prop.put("mail.smtp.port","587");
+
+            javax.mail.Session mailSession = javax.mail.Session.getInstance(prop,
+                    new Authenticator() {
+                        @Override
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(senderEmail, passwordEmail);
+                        }
+                    }
+            );
+
+            try {
+                Message message = new MimeMessage(mailSession);
+                message.setFrom(new InternetAddress(senderEmail));
+                message.setRecipient(Message.RecipientType.TO,
+                        new InternetAddress("ngomanhson2004txpt@gmail.com"));
+                message.setSubject("Create new a student!");
+                message.setText("<h1>This is a test email</h1>");
+
+                // Send mail
+                Transport.send(message);
+            }catch (Exception e) {
+                System.out.print(e.getMessage());
+            }
         }
         resp.sendRedirect("list-student");
     }
